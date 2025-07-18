d74187fd33772971f695a9b8c20d0b8c3729d7ed
drplx <drplx@drplx.ru>
2025-07-18 14:13:52 +0700

    new logger+middleware

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,21 +2,32 @@ package main
 
 import (
 	"api-service/internal/handler"
+	"api-service/internal/middleware"
 	"log"
+	"log/slog"
 	"os"
 
 	"github.com/gofiber/fiber/v3"
 )
 
 func main() {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	}))
+
+	slog.SetDefault(logger)
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
 	}
 	app := fiber.New()
+	app.Use(middleware.SlogLogger())
 	handler.RegisterTaskRoutes(app)
-	log.Printf("Starting API service on port %s", port)
+
+	slog.Info("Service started", "port", 8080)
+	slog.Warn("Low disk space", "disk", "/dev/sda1", "free_percent", 5)
+	slog.Error("Database connection failed", "error", "timeout")
 	log.Fatal(app.Listen(":" + port))
 
 }
