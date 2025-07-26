045db84777c7503481079595bc8a89c1a812d0ed
drplx <drplx@drplx.ru>
2025-07-26 16:02:25 +0700

    cors methods add

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
 )
 
 func main() {
@@ -22,6 +23,14 @@ func main() {
 		port = "3000"
 	}
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://my-samovar-to-do-list.duckdns.org/", "http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		AllowCredentials: true,
+		ExposeHeaders:    []string{"Authorization"},
+	}))
+
 	app.Use(middleware.SlogLogger())
 	handler.RegisterTaskRoutes(app)
 
